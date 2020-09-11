336327e0158f23c46746e8ac880ee7e4bb27771c
BOBOWang <bbw1996@qq.com>
2020-09-12 00:03:19 +0800

    将 create 封装到 recordListModel.ts

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -37,14 +37,12 @@
     };
 
     saveRecord() {
-      const record2: RecordItem = recordListModel.clone(this.record);
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
+      recordListModel.create(this.record);
     }
 
     @Watch('recordList')
     onRecordListChange() {
-      recordListModel.save(this.recordList);
+      recordListModel.save();
     }
   }
 </script>
@@ -53,7 +51,8 @@
     display: flex;
     flex-direction: column-reverse;
   }
-  .notes{
+
+  .notes {
     padding: 12px 0;
   }
 </style>
