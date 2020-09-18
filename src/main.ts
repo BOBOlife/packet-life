88039d47836d80a2349f9ceaa1006b810722d6ec
BOBOWang <bbw1996@qq.com>
2020-09-18 15:25:14 +0800

    解决一些小bug

--- src/main.ts
+++ src/main.ts
@@ -25,11 +25,10 @@ window.onload = function () {
     window.scrollTo(0, 10000);
   }, 0);
 };
-
 if (document.documentElement.clientWidth > 500) {
   window.alert('请使用手机打开本页面，以保证浏览效果');
   const img = document.createElement('img');
-  img.src = '/qrcode.png';
+  img.src = '../public/qrcode.png';
   img.style.position = 'fixed';
   img.style.left = '50%';
   img.style.top = '50%';
