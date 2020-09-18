ece2d28af8c25e02cbae70601b196d061fbadf92
BOBOWang <bbw1996@qq.com>
2020-09-18 14:35:05 +0800

    保证用户用手机打开页面，否则就提示用户

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/packet-life-website/'
+    : '/',
   lintOnSave: false,
 
   chainWebpack: config => {
@@ -12,7 +15,7 @@ module.exports = {
       .include.add(dir).end()//包含icons目录
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()
       .use('svgo-loader').loader('svgo-loader')
-      .tap(options => ({...options,plugins:[{removeAttrs:{attr:'fill'}}]})).end()
+      .tap(options => ({...options, plugins: [{removeAttrs: {attr: 'fill'}}]})).end()
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir)//其它 svg loader 排除 icons目录
   }
